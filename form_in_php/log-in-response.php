f69ca327dc0e72a0f99811f643b8e5c0f5db6525
Fabio <ciaociao@gmail.com>
2023-02-06 12:37:37 +0100

    pre-modifiche

--- form_in_php/log-in-response.php
+++ form_in_php/log-in-response.php
@@ -1,6 +1,14 @@
 <h1> Sono la risposta (RESPONSE)</h1>
 <?php
 echo "<pre>";
+print_r($_GET);
+echo "<pre>";
+
+echo "<pre>";
 print_r($_POST);
 echo "<pre>";
+
+
+echo "La tua email è <br>";
+echo "<strong>".$_POST['email']."</strong>";
 ?>
