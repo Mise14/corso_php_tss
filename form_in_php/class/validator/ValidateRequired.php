40014ca0c0b2879c8a65cb1ff45df8cc3eccef04
Fabio <ciaociao@gmail.com>
2023-02-21 17:42:48 +0100

    validate-date-def

--- form_in_php/class/validator/ValidateRequired.php
+++ form_in_php/class/validator/ValidateRequired.php
@@ -2,10 +2,10 @@
 
 class ValidateRequired
 {
-
+    /*
     public function isEmpty($data)
     {
-        /*empty da 0 (true) se è vuota, 1 (false) se non è vuota */
+        /*empty da 0 (true) se è vuota, 1 (false) se non è vuota 
         foreach ($data as $i => $elementi) {
             if (empty($elementi)) {
                 echo "Test NON superato per :[$i] perchè vuoto <br> ";
@@ -13,5 +13,17 @@ class ValidateRequired
                 echo " Test superato per :[$i] perche' pieno <br> ";
             }
         }
+    }
+    */
+
+    public function isValid($value)
+    {
+        $valueWithoutSpace = trim(strip_tags($value));
+
+        //se inserisco una stringa con soli spazi, e con trim mi ritorna una stringa vuota -> ritorna false
+        if($valueWithoutSpace == ''){
+            return false;
+        }
+        return $valueWithoutSpace;
     }
 }
--- form_in_php/test/test_validazione_date.php
+++ form_in_php/test/test_validazione_date.php
@@ -2,18 +2,56 @@
 
 // $files = scandir("./form_in_php/class/validator");
 
-
+require "/xampp/htdocs/corso_php_tss/form_in_php/class/validator/Validable.php";
 require "/xampp/htdocs/corso_php_tss/form_in_php/class/validator/ValidateDate.php";
-/*primi due valori sono vuoti 3-4 dovrebbe risp non superato */
-$date = ['20#01#1990', "1/02/1190", "", "31-01-1990", "20.01.1990", "20-1-19999"];
 
 
-$v = new ValidateDate();
+$testCases = [
+    [
+        'input' => '           ',
+        'expected' => false
+     ],
+     [
+        'input' => '20#01#1990',
+        'expected' => false
+     ],
+     [
+        'input' => '1/02/1190',
+        'expected' => false
+     ],
+     [
+        'input' => '',
+        'expected' => false
+     ],
+     [
+        'input' => '31-01-1990',
+        'expected' => false
+     ],
+     [
+        'input' => '20.01.1990',
+        'expected' => false
+     ],
+     [
+        'input' => '20-1-19999',
+        'expected' => false
+     ],
+     [
+        'input' => '31/01/1990',
+        'expected' => true
+     ]
+     ];
 
-foreach ($date as $i => $data) {
-    if ($v->isValid($data)) {
-        echo "[$i] vero";
-    } else {
-        echo "[$i] falso";
-    }
+
+foreach ($testCases as $key => $test){
+    $input = $test['input'];
+    $expected = $test['expected'];
+
+    $v = new ValidateDate();
+
+   if($v -> isValid($input) != $expected){
+        echo "\nTest non superato mi aspettavo:";
+        var_dump($expected);
+        echo "\nma ho trovato:";
+        var_dump($v->isValid($input));
+   };
 }
