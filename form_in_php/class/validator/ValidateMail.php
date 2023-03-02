4fb5765ba6bcfb4fba1ac55d79fa384ef9738333
Fabio <ciaociao@gmail.com>
2023-03-02 12:20:51 +0100

    def-lezione

--- form_in_php/class/validator/ValidateMail.php
+++ form_in_php/class/validator/ValidateMail.php
@@ -4,6 +4,8 @@ class ValidateMail implements Validable {
 
 
     public function isValid(mixed $email) : bool {
+        // $strip_tag = strip_tags($value);
+        // $valueWithoutSpace = trim($strip_tag);
         return filter_var($email,FILTER_VALIDATE_EMAIL);
     }
 
