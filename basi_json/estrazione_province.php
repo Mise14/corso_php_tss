f09376faa0f9f3eaa3daf54c1add6367c57e1431
Fabio <ciaociao@gmail.com>
2023-03-08 12:26:00 +0100

    def

--- basi_json/estrazione_province.php
+++ basi_json/estrazione_province.php
@@ -10,14 +10,11 @@ $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME;
 try {
     $conn = new PDO($dsn,DB_USER,DB_PASSWORD);
    // $conn->query('TRUNCATE TABLE provincia');
-
     foreach($province_object as $provincia) {
         $regione = $provincia->regione;
         $nome_provincia = addslashes($provincia->nome);
         $sigla_provincia = addslashes($provincia->sigla);
-
         $regione = $conn->query("SELECT regione_id FROM regione WHERE nome =\"$regione\"")->fetchColumn();
-
         $sql = "INSERT INTO provincia (nome,sigla,regione_id) VALUES('$nome_provincia','$sigla_provincia','$regione');";
         echo $sql ."\n";
         $conn->query($sql);
